0f4a942edd157c085d4b56e432a3902fa5082da5
Jareg123 <47992044+Jareg123@users.noreply.github.com>
2019-04-22 23:46:49 +0500

    Update Semux-parser.py

--- Semux-parser.py
+++ Semux-parser.py
@@ -1,14 +1,16 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 import csv
 import datetime
+
 ir = 0
 while ir < 2:
     ir += 1
     url = 'https://semux.info/delegates'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
-    name_delegat_all = soup.find_all('td' , class_='country')
+    name_delegat_all = soup.find_all('td', class_='country')
     
     spisok3 = []
     price1 = soup.findAll('tr')
@@ -29,41 +31,46 @@ while ir < 2:
 
     for i in spisok3:
         pobeda = (i.replace('Validator', ' ').split())
-        pobeda2 =(pobeda[0] + ' ' + pobeda[1]).split()
+        pobeda2 = (pobeda[0] + ' ' + pobeda[1]).split()
         summa.append(pobeda[1])
         data.append(pobeda2)
-    
+    ### ЗАПИСЬ В ФАИЛ ЕКСЕЛЬКУ###
     with open('top100.csv', 'w') as fp:
         writer = csv.writer(fp, delimiter=';')
         # writer.writerow(["your", "header", "foo"])  # write header
         writer.writerows(data)
     jopa = 0
+
     
     for i in summa:
-        c = i.split(',')
-        try:
-            d = int(c[0]+c[1])
-            jopa += d
-        except IndexError:
-            pass
+        b = int(i.replace(',', ''))
+        jopa += b
     
     time = datetime.datetime.now()
     time = str(time)
     
-    f = open('SumDelegate.txt','a')
+    f = open('SumDelegate2.txt', 'a')
     jopa = str(jopa)
 
-
-    file = open('SumDelegate.txt').read().splitlines()
+    file = open('SumDelegate2.txt').read().splitlines()
     
-    bilo = int(file[-2][:8])
-    stalo = int(file[-1][:8])
-    ostatok = stalo - bilo
-    print(file[-1][:8], file[-2][:8])
+    if os.stat("SumDelegate2.txt").st_size == 0:
+        file = open('SumDelegate2.txt', 'a')
+        f.write("8443573 SEM 2019-04-22 23:12:31.499806  Top 100: Lost\Arrived: 0." + '\n')
+        f.write("8443573 SEM 2019-04-22 23:12:31.499806  Top 100: Lost\Arrived: 0." + '\n')
+    try:
+        bilo = int(file[-2][:8])
+        stalo = int(file[-1][:8])
+        print(bilo)
+        print(stalo)
+        Ostalos = stalo - bilo
 
 
-    file = open('SumDelegate.txt','a')
-    f.write(jopa  + ' '+ 'SEM ' + time + '  Top 100: Lost\Arrived: ' + str(ostatok) + '.' + '\n')
+    except ValueError:
+        pass
+    except IndexError:
+        pass
+
+    file = open('SumDelegate2.txt', 'a')
+    f.write(jopa + ' ' + 'SEM ' + time + '  Top 100: Lost\Arrived: ' + str(Ostalos) + '.' + '\n')
     f.close()
-
-
